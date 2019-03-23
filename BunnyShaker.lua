344793446ca0fe6fc3ed8cf34439f9a629640074
datagutten <datagutten@datagutten.net>
2019-03-23 17:25:30 +0100

    Show remaining time of "Srung!" buff

--- BunnyShaker.lua
+++ BunnyShaker.lua
@@ -3,8 +3,16 @@ EventFrame:RegisterEvent('PLAYER_TARGET_CHANGED')
 EventFrame:RegisterEvent('ADDON_LOADED')
 --https://us.battle.net/forums/en/wow/topic/3595346852
 EventFrame:RegisterEvent('UPDATE_MOUSEOVER_UNIT')
+EventFrame:RegisterEvent('UNIT_SPELLCAST_SUCCEEDED')
 
 local completed_classes = {}
+local bunny_debug = false
+local buff_name = 'Sprung!'
+local unit = 'target'
+local race, gender, level, race_text
+local valid_unit = false
+local has_target = false
+
 --achievement 2422
 function colorize(text, color)
     return string.format('|cFF%s%s|r', color, text)
@@ -32,7 +40,15 @@ function find_buff(find_name, unit)
 end
 
 function buff_expiry(expirationTime)
-    return string.format("%.2f minutes left.", expirationTime - GetTime()/60)
+    local time_left = expirationTime - GetTime()
+    if time_left < 60 then
+        return string.format("%.0f seconds left.", time_left)
+    else
+        local minutes = math.floor(time_left/60)
+        local seconds = math.floor(time_left - (minutes*60))
+        -- return string.format("%.2f minutes left.", time_left/60)
+        return string.format("%d:%d minutes left.", minutes, seconds)
+    end
 end
 
 function bunny_progress()
@@ -40,11 +56,18 @@ function bunny_progress()
         local criteriaString, criteriaType, completed, quantity, reqQuantity,
         charName, flags, assetID, quantityString, criteriaID = GetAchievementCriteriaInfo(2422, i)
         completed_classes[criteriaString] = completed
-        --if completed then
-        --    DEFAULT_CHAT_FRAME:AddMessage(string.format('%s', colorize(criteriaString, '00ff00')))
-        --else
-        --    DEFAULT_CHAT_FRAME:AddMessage(string.format('%s', colorize(criteriaString, 'ff0000')))
-        --end
+    end
+end
+function completed_debug()
+    for i=1, GetAchievementNumCriteria(2422) do
+        local criteriaString, criteriaType, completed, quantity, reqQuantity,
+        charName, flags, assetID, quantityString, criteriaID = GetAchievementCriteriaInfo(2422, i)
+
+        if completed_classes[criteriaString] then
+            DEFAULT_CHAT_FRAME:AddMessage(string.format('%s', colorize(criteriaString, '00ff00')))
+        else
+            DEFAULT_CHAT_FRAME:AddMessage(string.format('%s', colorize(criteriaString, 'ff0000')))
+        end
     end
 end
 
@@ -55,50 +78,85 @@ function race_image(race, gender)
     return string.format("Interface/CHARACTERFRAME/TEMPORARYPORTRAIT-%s-%s.PNG",gender, race:gsub(' ', ''))
 end
 
-function gender_check(UnitId)
-    if not UnitId then
-        UnitId = "target"
-    end
-    BunnyMakerTooltip:Hide()
+function update_tooltip()
+    local buff, icon, count, _, duration, expirationTime = find_buff(buff_name, unit)
 
-    if not UnitIsPlayer(UnitId) then
-        return
+    if not has_target and not UnitIsPlayer('mouseover') then
+        valid_unit = false
     end
-    local gender = UnitSex(UnitId)
-    local race = UnitRace(UnitId)
-    --Race is not an objective or is completed
-    if completed_classes[race] ~= false then
-        return
-    end
-    local level = UnitLevel(UnitId)
 
-    -- DEFAULT_CHAT_FRAME:AddMessage(string.format('Gender: %d Race: %d, Level %d', gender, race, level))
-    if level >= 18 then
-        -- DEFAULT_CHAT_FRAME:AddMessage(string.format('Target is female %s of level %s', race, level))
-        local race_text = string.format('%s level %s', race, level)
-        BunnyMakerTooltip_RaceText:SetText(race_text);
+    if not valid_unit then
+        BunnyMakerTooltip:Hide()
+    else
+        BunnyMakerTooltip:Show()
+    end
+    BunnyMakerTooltip_RaceText:SetText(race_text);
+    if buff then
+        BunnyMakerTooltip_BuffText:SetText(buff_expiry(expirationTime))
+        BunnyMakerTooltip_RaceIcon:SetTexture('Interface/ICONS/INV_Misc_Roses_01.PNG')
+    else
+        BunnyMakerTooltip_BuffText:SetText()
         if gender == 3 then
             BunnyMakerTooltip_RaceIcon:SetTexture(race_image(race, 'female'))
         elseif gender == 2 then
             BunnyMakerTooltip_RaceIcon:SetTexture(race_image(race, 'male'))
         end
-        --TODO: Add buff check to tooltip
-        buff = find_buff('Sprung!', UnitId)
-
-        BunnyMakerTooltip:Show();
-
     end
 end
 
+function unit_info(UnitId)
+    if not UnitIsPlayer(UnitId) then
+        valid_unit = false
+        return
+    end
+    level = UnitLevel(UnitId)
+    if level < 18 then
+        valid_unit = false
+        return
+    end
+    --Race is not an objective or is completed
+    if completed_classes[race] ~= false and bunny_debug == false then
+        valid_unit = false
+        return
+    end
+
+    valid_unit = true
+    unit = UnitId
+    gender = UnitSex(UnitId)
+    race = UnitRace(UnitId)
+    race_text = string.format('%s level %s', race, level)
+    if bunny_debug then
+        print('Unit updated:', race_text)
+    end
+end
+
+EventFrame:SetScript('OnUpdate', update_tooltip)
 EventFrame:SetScript("OnEvent", function(self, event,...)
     if event == 'PLAYER_TARGET_CHANGED' then
-        gender_check()
+        unit_info('target')
+        if bunny_debug then
+            print('Target changed, is the target valid?', valid_unit)
+        end
+        if UnitIsPlayer("target") then
+            has_target = true
+        else
+            has_target = false
+        end
     elseif event == 'UPDATE_MOUSEOVER_UNIT' then
         --Only use mouseover if no player is targeted
+        if bunny_debug then
+            print('UPDATE_MOUSEOVER_UNIT')
+        end
         if not UnitIsPlayer("target") then
-            gender_check("mouseover")
+            unit_info('mouseover')
         end
     elseif event == 'ADDON_LOADED' then
         bunny_progress()
+    elseif event == 'UNIT_SPELLCAST_SUCCEEDED' then
+        local target, _, spell = ...
+        if spell == 61815 then
+            print(string.format('Sprung! cast on %s', target))
+            --TODO: Update progress
+        end
     end
 end)
