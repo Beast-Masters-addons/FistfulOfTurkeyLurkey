ef7d514f12a2b63f4b6d5e3c22de820236395e54
datagutten <datagutten@datagutten.net>
2023-02-13 22:06:37 +0100

    Add missing data files

---
+++ data/achievements.lua
@@ -0,0 +1,34 @@
+_G.fistful_achievements = {
+    fistful = {
+        ["achievement"] = 1699,
+        ["min_level"] = 18,
+        ["spell"] = 27571,
+        ["race"] = true,
+        ["class"] = true,
+        ["buff"] = 27571,
+        ["item"] = 22218, -- Handful of Rose Petals
+        ["item_icon"] = 133851,
+    },
+
+    bunnymaker = {
+        ["achievement"] = 2422,
+        ["min_level"] = 18,
+        ["type"] = 'race',
+        ["buff"] = 61815,
+        ["race"] = true,
+        ["class"] = false,
+    },
+
+    check_your_head = {
+        ["achievement"] = 291,
+        ["buff"] = 44212,
+        ["race"] = true,
+        ["class"] = false
+    },
+
+    let_it_snow = {
+        ["achievement"] = 1687,
+        ["race"] = true,
+        ["class"] = true
+    },
+}
